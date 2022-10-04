7869a987f262dd8a8a80e4a8eaf4fb2aaab8e177
Luis Miguel Sanchez Pinilla <luism.sanchezp@autonoma.edu.co>
2022-10-04 01:39:57 -0400

    Add JSON responses for handling API Exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,6 +5,9 @@ namespace App\Exceptions;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Log;
+
 class Handler extends ExceptionHandler
 {
     /**
@@ -47,4 +50,25 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    public function handleApiExceptions($request, $exception)
+    {
+        if($exception instanceof ModelNotFoundException)
+        {
+            return response()->json(['error' => 'Model Not Found'], 404);
+        }
+        Log::warning("[Handler.handleApiExceptions] API Exception type '" . get_class($exception) . "' not handled.");
+        
+        return parent::render($request, $exception);
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if($request->expectsJson())
+        {
+            return $this->handleApiExceptions($request, $exception);
+        }
+        return parent::render($request, $exception);
+    }
+
 }
