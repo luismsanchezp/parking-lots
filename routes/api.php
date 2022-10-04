8a842bdbb7a701c1d26a9eb427acc62642af975b
Luis Miguel Sanchez Pinilla <luism.sanchezp@autonoma.edu.co>
2022-10-04 00:20:14 -0400

    Add routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\api\v1\CustomerController;
+use App\Http\Controllers\api\v1\ParkingLotController;
+use App\Http\Controllers\api\v1\ParkingSpotController;
+use App\Http\Controllers\api\v1\TicketController;
+use App\Http\Controllers\api\v1\UserController;
+use App\Http\Controllers\api\v1\VehicleController;
+use App\Http\Controllers\api\v1\VehicleTypeController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +21,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::apiResource('v1/customers', CustomerController::class);
+Route::apiResource('v1/parkinglots', ParkingLotController::class);
+Route::apiResource('v1/parkingspots', ParkingSpotController::class);
+Route::apiResource('v1/tickets', TicketController::class);
+Route::apiResource('v1/users', UserController::class);
+Route::apiResource('v1/vehicles', VehicleController::class);
+Route::apiResource('v1/vehicletypes', VehicleTypeController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
